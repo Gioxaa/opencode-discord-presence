9391e58a83531365d584dd083c9478c71231643c
Puri12 <tkdqja9812@gmail.com>
2026-02-01 03:41:56 +0900

    chore: update @opencode-ai/plugin to 1.1.47 and simplify exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 import { OpenCodeDiscordPresence } from "./plugin.js"
 
 export default OpenCodeDiscordPresence
-export { getConfig } from "./config.js"
-export type { DiscordPresenceOptions, Language } from "./types/index.js"
